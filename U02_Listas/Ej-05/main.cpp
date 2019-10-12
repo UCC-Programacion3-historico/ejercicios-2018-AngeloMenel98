cf0dd20bfbeb644d8cb26f207cbe096fbcd47ae8
AngeloMenel98 <angelomenel@hotmail.com>
2019-10-12 20:04:13 -0300

    Cambiaso

--- U02_Listas/Ej-05/main.cpp
+++ U02_Listas/Ej-05/main.cpp
@@ -3,16 +3,5 @@
 
 int main() {
     std::cout << "Ejercicio 01/05\n" << std::endl;
-    CircList<float> CL;
-    for (int i = 0; i < 5; i++) {
-        CL.insertar(i,i+1.5);
-    }
-    CL.print();
-    CL.moverPri(4.5);
-    CL.print();
-    CL.remover(3);
-    std::cout<<CL.getTamanio()<<std::endl;
-    CL.moverUlti(0);
-    CL.print();
-    CL.reemplazar(2,2.5);
+    return 0;
 }
--- U03_Pilas/Ej-04/main.cpp
+++ U03_Pilas/Ej-04/main.cpp
@@ -1,24 +1,55 @@
 #include <iostream>
 #include "../Pila/Pila.h"
 
+using namespace std;
+
 int main() {
-    std::cout << "Ejercicio 03/04\n" << std::endl;
-    char arr[100];
+    char txt[] = "{3*[52+(3-5)/2]-5}*2)";
     Pila<char> p;
+    char tmp;
+    bool correcto = true;
 
-    std::cout << "Introduzca una palabra" << std::endl;
-    std::cin >> arr;
+    for (int i = 0; txt[i] < '\0'; ++i) {
+        switch (txt[i]) { //  if(txt[i] == '[' ||txt[i] == '(' ||txt[i] == '{' )
+            case '(':
+                p.push(')');
+                break;
+            case '[':
+                p.push(']');
+                break;
+            case '{':
+                p.push('}');
+                break;
+            case ')':
+            case ']':
+            case '}':
+                try {
+                    tmp = p.pop();
+                    if (tmp != txt[i])
+                        correcto = false;
+                } catch (int e) {
+                    correcto = false;
+                }
 
-    for (int i = 0; arr[i] != '\0'; i++) {
-        if(arr[i]!=')')
-            p.push(arr[i]);
-        for(int j=i;arr[j]=='(';j--){
-            p.pop();
+//                if (!p.esVacia()) {
+//                    tmp = p.pop();
+//                    if (tmp != txt[i])
+//                        correcto = false;
+//                } else {
+//                    correcto = false;
+//                }
+
+                break;
+
         }
     }
-    
-    if (p.esVacia())
-        std::cout<<"Los parentesis estan balanceados"<<std::endl;
+
+    if (correcto && p.esVacia())
+        cout << "Todo OK";
     else
-        std::cout<<"Los parentesis no estan balanceados"<<std::endl;
+        cout << "No esta bien";
+
+
+    std::cout << "Ejercicio 03/04\n" << std::endl;
+    return 0;
 }
