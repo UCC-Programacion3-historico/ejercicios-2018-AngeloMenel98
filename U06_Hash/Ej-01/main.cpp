24711c6d979c32f27e773aa454f38b7ea7cdc59c
AngeloMenel98 <angelomenel@hotmail.com>
2019-12-08 17:57:25 -0300

    Hash

--- U06_Hash/Ej-01/main.cpp
+++ U06_Hash/Ej-01/main.cpp
@@ -1,6 +1,26 @@
 #include <iostream>
+#include <string>
+#include "../HashMap/HashMapConColi.h"
+
+using namespace std;
+
+
+unsigned int miHashF(string clave);
+
 
 int main() {
-    std::cout << "Ejercicio 05/01\n" << std::endl;
-    return 0;
+    HashMap<int, string> th(13);
+
+    th.put(13, "Hola");
+    th.put(325, "Chau");
+    th.print();
+}
+
+unsigned int miHashF(string clave) {
+    unsigned int idx = 1;
+    for (int i = 0; i < clave.length(); ++i) {
+        idx *= clave[i] * clave[i];
+    }
+
+    return idx;
 }
